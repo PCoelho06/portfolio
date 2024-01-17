dad5435fda0d7b3f3f90eca2cdcb9cbcbfa86a0a
PierreDSC <p.coelho@hotmail.fr>
2024-01-17 16:58:11 +0100

    Animated toggler in navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,17 @@
 let header = document.getElementsByTagName('header')[0]
 let navbarToggler = document.querySelector('button.navbar-toggler')
+let togglerUp = document.querySelector('.navbar-toggler-up')
+let togglerMiddle = document.querySelector('.navbar-toggler-middle')
+let togglerBottom = document.querySelector('.navbar-toggler-bottom')
+let navbarNav = document.querySelector('#navbarNav')
 
 navbarToggler.addEventListener("click", _ => {
-    header.classList.toggle("header-bg")
+    Array.from(navbarToggler.children).forEach(element => {
+        element.classList.toggle('bg-light')
+    });
+    
+    navbarNav.classList.toggle('collapse')
+    togglerUp.classList.toggle('navbar-toggler-up-animation')
+    togglerMiddle.classList.toggle('navbar-toggler-middle-animation')
+    togglerBottom.classList.toggle('navbar-toggler-bottom-animation')
 })
--- view/layout.php
+++ view/layout.php
@@ -4,42 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/b3e17d2f75.js" crossorigin="anonymous"></script>
     <title><?= $title ?> - Lapinou.tech</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
     <script src="assets/js/navbar.js" defer></script>
 </head>
 
-<body class="d-flex flex-column h-100">
+<body class="d-flex flex-column">
     <header>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">
-                    <img src="assets/img/logo.svg" alt="Logo" class="d-inline-block align-text-top logo">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <div class="navbar-toggler-up"></div>
-                    <div class="navbar-toggler-middle"></div>
-                    <div class="navbar-toggler-down"></div>
-                </button>
-                <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">projets</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">à propos</a>
-                        </li>
-                    </ul>
-                </div>
+        <nav class="navbar">
+            <a class="navbar-brand" href="/">
+                <img src="assets/img/logo.svg" alt="Logo" class="d-inline-block align-text-top logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="navbar-toggler-up"></div>
+                <div class="navbar-toggler-middle"></div>
+                <div class="navbar-toggler-bottom"></div>
+            </button>
+            <div class="collapse text-center" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">projets</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">à propos</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </header>
